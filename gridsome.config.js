24d065349670c6ddb08869814fc7c715499fcf71
Matthew Beaumont <matt@beaumontwebdev.com>
2020-05-01 20:47:06 -0400

    initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,15 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Megan Shumate Beaumont, Clarinet and Saxophone Teacher',
+  siteUrl: 'https://www.meganbeaumont.com',
+  plugins: [
+    {
+      use: 'gridsome-plugin-tailwindcss'
+    }
+  ]
+}
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      heading: ['Playfair Display', 'serif'],
+      body: ['Cabin', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        primary: {
+          100: '#0080B3',
+          300: '#00B7FF'
+        },
+        secondary: {
+          100: '#B35E00',
+          300: '#FF8600'
+        }
+      }
+
+    },
+  },
+  variants: {},
+  plugins: [],
+}
